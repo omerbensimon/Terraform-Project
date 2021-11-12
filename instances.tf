76650091bb1736bd38ff2fa54678f864089457bb
omerbensimon <omerbensimon3005@gmail.com>
2021-11-12 14:58:46 +0200

    nat update

--- instances.tf
+++ instances.tf
@@ -1,32 +1,112 @@
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
-}
+# Create VPC
+# terraform aws create vpc
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.vpc-cidr
+  instance_tenancy     = "default"
+  enable_dns_hostnames = true
 
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
-  //count = 2
-  name  = "nginx-server"
-  ports {
-    internal = 80
-    external = "8080"
-  }
-  volumes {
-    container_path = "/usr/share/nginx/html"
-    host_path      = "/tmp/tutorial/www"
-    read_only      = true
+  tags = {
+    Name = "Test VPC"
   }
 }
 
-# output "aws-nginx-ip" {
-#   value = aws_instance.webservers.public_ip
-# }
+# Create Internet Gateway and Attach it to VPC
+# terraform aws create internet gateway
+resource "aws_internet_gateway" "internet-gateway" {
+  vpc_id = aws_vpc.vpc.id
 
-resource "aws_instance" "webservers" {
-	count = 2 
-	ami = var.webservers_ami
-	instance_type = "${var.instance_type}"
-	security_groups = ["${aws_security_group.webservers.id}"]
-	subnet_id = "${aws_subnet.private-subnet-1.id}"
-	user_data = file("install_httpd.sh")
+  tags = {
+    Name = "Test IGW"
+  }
+}
+
+# Allocate Elastic IP Address (EIP 1)
+# terraform aws allocate elastic ip
+resource "aws_eip" "eip-for-nat-gateway-1" {
+  vpc = true
+
+  tags = {
+    Name = "EIP 1"
+  }
+}
+
+# Create Public Subnet 1
+# terraform aws create subnet
+resource "aws_subnet" "public-subnet-1" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.public-subnet-1-cidr
+  availability_zone       = "eu-west-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public Subnet 1"
+  }
+}
+
+# Create Route Table and Add Public Route
+# terraform aws create route table
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet-gateway.id
+  }
+
+  tags = {
+    Name = "Public Route Table"
+  }
+}
+
+# Associate Public Subnet 1 to "Public Route Table"
+# terraform aws associate subnet with route table
+resource "aws_route_table_association" "public-subnet-1-route-table-association" {
+  subnet_id      = aws_subnet.public-subnet-1.id
+  route_table_id = aws_route_table.public-route-table.id
+}
+
+# Create Private Subnet 1
+# terraform aws create subnet
+resource "aws_subnet" "private-subnet-1" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.private-subnet-1-cidr
+  availability_zone       = "eu-west-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "Private Subnet 1 | App Tier"
+  }
+}
+
+# Create Nat Gateway 1 in Public Subnet 1
+# terraform create aws nat gateway
+resource "aws_nat_gateway" "nat-gateway-1" {
+  allocation_id = aws_eip.eip-for-nat-gateway-1.id
+  subnet_id     = aws_subnet.public-subnet-1.id
+
+  tags = {
+    Name = "Nat Gateway private Subnet 1"
+  }
+}
+
+# Create Private Route Table 1 and Add Route Through Nat Gateway 1
+# terraform aws create route table
+resource "aws_route_table" "private-route-table-1" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat-gateway-1.id
+  }
+
+  tags = {
+    Name = "Private Route Table 1"
+  }
+}
+
+# Associate Private Subnet 1 with "Private Route Table 1"
+# terraform aws associate subnet with route table
+resource "aws_route_table_association" "private-subnet-1-route-table-association" {
+  subnet_id      = aws_subnet.private-subnet-1.id
+  route_table_id = aws_route_table.private-route-table-1.id
 }
